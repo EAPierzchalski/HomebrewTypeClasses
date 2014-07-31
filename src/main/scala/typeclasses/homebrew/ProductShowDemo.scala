258bfb06211bab8dfcd8af370f85142faf20ad6b
EAPierzchalski <e.a.pierzchalski@gmail.com>
2014-07-31 22:31:54 +1000

    Add examples

---
+++ src/main/scala/typeclasses/homebrew/ProductShowDemo.scala
@@ -0,0 +1,47 @@
+package com.tomorrowlandenterprises.experimental.shapeless.typeclasses.homebrew
+
+import shapeless._
+
+/**
+ * Created by eap on 7/31/14.
+ */
+object ProductShowDemo {
+  sealed trait T
+  case class A(n: Int) extends T
+  case class B(wop: String, n: Int) extends T
+  sealed trait TT extends T
+  case class C(d: Double) extends TT
+
+  sealed trait Rec
+  case class RNil(n: Int) extends Rec
+  case class RCons(s: String, rest: Rec) extends Rec
+
+  sealed trait Inj
+  case class InjInt(n: Int) extends Inj
+  case class InjString(s: String) extends Inj
+
+  object InjInt {
+    implicit def showInjInt = Show[InjInt] {
+      case InjInt(n) => s"--injected! $n--"
+    }
+  }
+
+  def main(args: Array[String]) {
+    import Show._
+    import ShowInstances.auto._
+    //reaches within tuples to find show instances
+    println(((B("magic", 12), A(14)): (T, T)).show)
+
+    //fails to handle recursive types: 'diverging implicit expansion'.
+    //presumably using Lazy[_] will help, but whatever I tried wasn't very
+    //successful.
+    //println((RCons("hi", RCons("lo", RNil(12))): Rec).show)
+
+    //successfully uses injected instances, but only after import.
+    //this could be made more elegant by refactoring some of the
+    //typeclass code into a hierarchy with a final instantiation step
+    //where custom instances could be injected.
+    import InjInt._
+    println((InjInt(5): Inj).show)
+  }
+}
---
+++ src/main/scala/typeclasses/homebrew/TypeClass.scala
@@ -0,0 +1,93 @@
+package com.tomorrowlandenterprises.experimental.shapeless.typeclasses.homebrew
+
+import shapeless._
+import shapeless.record._
+
+/**
+ * Created by eap on 7/31/14.
+ */
+
+trait TypeClass[C[_]] {
+  def emptyProduct: C[HNil]
+  def productInstance[H, T <: HList](
+    name: String,
+    CH: => C[H],
+    CT: => C[T]): C[H :: T]
+  def emptyCoproduct: C[CNil]
+  def coproductInstance[L, R <: Coproduct](
+    name: String,
+    CL: => C[L],
+    CR: => C[R]): C[L :+: R]
+  def project[A, B](
+    instance: C[B],
+    to: A => B,
+    from: B => A): C[A]
+
+  trait InstanceFor[A] extends DepFn0 {
+    type Inner
+    final type Out = C[Inner]
+  }
+
+  type Aux[A, Inner0] = InstanceFor[A] {
+    type Inner = Inner0
+  }
+
+  implicit def hnilCase[In <: HNil]: Aux[In, HNil] =
+    new InstanceFor[In] {
+      type Inner = HNil
+      override def apply(): Out = emptyProduct
+    }
+
+  implicit def productCase[Label <: Symbol, Head, Tail <: HList, TailOut <: HList](
+    implicit witnessLabel: Witness.Aux[Label],
+    headInstance: C[Head],
+    tailInstance: Aux[Tail, TailOut]): Aux[FieldType[Label, Head] :: Tail, Head :: TailOut] =
+    new InstanceFor[FieldType[Label, Head]:: Tail] {
+      type Inner = Head :: TailOut
+      override def apply(): Out =
+        productInstance(
+          name = witnessLabel.value.name,
+          CH = headInstance,
+          CT = tailInstance()
+        )
+    }
+
+  implicit def cnilCase[In <: CNil]: Aux[In, CNil] =
+    new InstanceFor[In] {
+      type Inner = CNil
+      override def apply(): Out = emptyCoproduct
+    }
+
+  implicit def coproductCase[Label <: Symbol, Left, Right <: Coproduct, TailOut <: Coproduct](
+    implicit witnessLabel: Witness.Aux[Label],
+    headInstance: C[Left],
+    tailInstance: Aux[Right, TailOut]): Aux[FieldType[Label, Left] :+: Right, Left :+: TailOut] =
+    new InstanceFor[FieldType[Label, Left]:+: Right] {
+      type Inner = Left :+: TailOut
+      override def apply(): Out =
+        coproductInstance(
+          name = witnessLabel.value.name,
+          CL = headInstance,
+          CR = tailInstance()
+        )
+    }
+
+  implicit def genericCase[A, LabelledRep, StrippedRep](
+    implicit labelledGenA: LabelledGeneric[A] { type Repr = LabelledRep },
+    bareGenA: Generic[A] { type Repr = StrippedRep },
+    labelledInstance: InstanceFor[LabelledRep] { type Inner = StrippedRep }): InstanceFor[A] { type Inner = A } =
+    new InstanceFor[A] {
+      type Inner = A
+      override def apply(): Out = project[A, StrippedRep](
+        instance = labelledInstance(),
+        to = bareGenA.to,
+        from = bareGenA.from
+      )
+    }
+
+  object auto {
+    implicit def derive[A](implicit instance: InstanceFor[A] { type Inner = A }): C[A] = instance()
+  }
+
+  def apply[A](implicit instance: InstanceFor[A] { type Inner = A }) = instance()
+}
