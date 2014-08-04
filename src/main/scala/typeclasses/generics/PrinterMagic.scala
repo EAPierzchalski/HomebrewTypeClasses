f2abf6246732ea21ec06521afc544ba8bd5edfc6
EAPierzchalski <e.a.pierzchalski@gmail.com>
2014-08-04 18:35:53 +1000

    Add some demos of the new snapshot shenanigans
    It seems that we need explicit product and coproduct generic instances
    for DepFn0's that use Generic[_] as an argument. On the plus side,
    Lazy[Generic[_]] works!

--- src/main/scala/typeclasses/generics/PrinterMagic.scala
+++ src/main/scala/typeclasses/generics/PrinterMagic.scala
@@ -1,12 +1,70 @@
 package typeclasses.generics
 
-import shapeless.DepFn0
+import shapeless._
 
 /**
  * Created by eap on 8/4/14.
  */
 object PrinterMagic {
-  trait PrinterInstance[A] extends DepFn0 {
+  trait Printer[A] extends DepFn0 {
     type Out = A => String
+  }
+
+  implicit def emptyProduct[In <: HNil]: Printer[In] =
+    new Printer[In] {
+      def apply() = _ => "HNil"
+    }
+
+  implicit def product[Head, Tail <: HList](
+    implicit tailPrinter: Printer[Tail]): Printer[Head :: Tail] =
+    new Printer[Head :: Tail] {
+      def apply() = {
+        case h :: t => s"${h.toString} :*: ${tailPrinter()(t)}"
+      }
+    }
+
+  implicit def genericProduct[A, Repr <: HList](
+    implicit gen: Generic.Aux[A, Repr],
+    reprPrinter: Printer[Repr]): Printer[A] =
+    new Printer[A] {
+      override def apply() = a => reprPrinter()(gen.to(a))
+    }
+
+  implicit def emptyCoproduct[In <: CNil]: Printer[In] =
+    new Printer[In] {
+      def apply() = throw new Exception("should never try and print CNil")
+    }
+
+  implicit def coproduct[Left, Right <: Coproduct](
+    implicit rightPrinter: Printer[Right]): Printer[Left :+: Right] =
+    new Printer[Left :+: Right] {
+      def apply() = {
+        case Inl(left) => left.toString
+        case Inr(right) => rightPrinter()(right)
+      }
+    }
+
+  implicit def genericCoproduct[A, Repr <: Coproduct](
+    implicit gen: Generic.Aux[A, Repr],
+    reprPrinter: Printer[Repr]): Printer[A] =
+    new Printer[A] {
+      def apply() = a => reprPrinter()(gen.to(a))
+    }
+
+  def apply[A](implicit printer: Printer[A]): A => String = printer()
+}
+
+object PrinterMagicDemo {
+  sealed trait T
+  case class A(n: Int) extends T
+  case class B(s: String, t: T) extends T
+  sealed trait TT extends T
+  case class C(d: Double) extends TT
+
+  def main(args: Array[String]) {
+    val bPrinter = PrinterMagic[B]
+    val tPrinter = PrinterMagic[T]
+    println(bPrinter(B("hi", A(12))))
+    println(tPrinter(A(12)))
   }
 }
--- src/main/scala/typeclasses/generics/SymbolRipper.scala
+++ src/main/scala/typeclasses/generics/SymbolRipper.scala
@@ -1,8 +1,97 @@
 package typeclasses.generics
 
+import shapeless.record.FieldType
+import shapeless._
+
 /**
  * Created by eap on 8/4/14.
  */
-object SymbolRipper {
 
+object GenericImplicits {
+  implicit def lazyLabGen[A](
+    implicit gen: LabelledGeneric[A]): Lazy[LabelledGeneric.Aux[A, gen.Repr]] = Lazy { gen }
+  implicit def lazyGen[A](
+    implicit gen: Generic[A]): Lazy[Generic.Aux[A, gen.Repr]] = Lazy { gen }
 }
+
+object SymbolRipper {
+  trait Ripper[A] extends DepFn0 {
+    type Out <: HList
+  }
+
+  implicit def emptyProduct[In <: HNil]: Ripper[In] =
+    new Ripper[In] {
+      type Out = HNil
+      def apply() = HNil
+    }
+
+  implicit def product[Label <: Symbol, Head, Tail <: HList](
+    implicit labelWitness: Lazy[Witness.Aux[Label]],
+    tailRipper: Lazy[Ripper[Tail]]): Ripper[FieldType[Label, Head] :: Tail] =
+    new Ripper[FieldType[Label, Head]:: Tail] {
+      type Out = String :: tailRipper.value.Out
+      def apply() = labelWitness.value.value.name :: tailRipper.value()
+    }
+
+  implicit def genericProduct[A, Repr <: HList](
+    implicit generic: Lazy[LabelledGeneric.Aux[A, Repr]],
+    reprRipper: Lazy[Ripper[Repr]]): Ripper[A] =
+    new Ripper[A] {
+      type Out = reprRipper.value.Out
+      def apply() = reprRipper.value()
+    }
+
+  implicit def emptyCoproduct[In <: CNil]: Ripper[In] =
+    new Ripper[In] {
+      type Out = HNil
+      def apply() = HNil
+    }
+
+  implicit def coproduct[Label <: Symbol, Left, Right <: Coproduct](
+    implicit labelWitness: Lazy[Witness.Aux[Label]],
+    tailRipper: Lazy[Ripper[Right]]): Ripper[FieldType[Label, Left] :+: Right] =
+    new Ripper[FieldType[Label, Left]:+: Right] {
+      type Out = String :: tailRipper.value.Out
+      def apply() = labelWitness.value.value.name :: tailRipper.value()
+    }
+
+  implicit def genericCoproduct[A, Repr <: Coproduct](
+    implicit generic: Lazy[LabelledGeneric.Aux[A, Repr]],
+    reprRipper: Lazy[Ripper[Repr]]): Ripper[A] =
+    new Ripper[A] {
+      type Out = reprRipper.value.Out
+      def apply() = reprRipper.value()
+    }
+
+  object auto {
+    implicit def derive[A](implicit ripper: Ripper[A]): ripper.Out = ripper()
+  }
+
+  def apply[A](implicit ripper: Ripper[A]): ripper.Out = ripper()
+}
+
+object RipperDemo {
+  sealed trait T
+  case class A(n: Int) extends T
+  case class B(s: String, t: T) extends T
+  sealed trait TT extends T
+  case class C(d: Double) extends TT
+
+  def main(args: Array[String]) {
+    import GenericImplicits._
+    import SymbolRipper._
+
+    def magic[A](implicit ripper: Ripper[A]): Unit = println(ripper())
+    magic[B]
+  }
+}
+
+/*
+  import shapeless._
+  sealed trait T
+  case class A(n: Int) extends T
+  case class B(s: String, t: T) extends T
+  sealed trait TT extends T
+  case class C(d: Double) extends TT
+
+ */ 
