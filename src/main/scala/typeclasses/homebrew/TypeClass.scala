b4ac639b5201db8022c78a775b943eaffe72891a
EAPierzchalski <e.a.pierzchalski@gmail.com>
2014-08-05 15:33:20 +1000

    Rename things to be less silly

--- src/main/scala/typeclasses/homebrew/TypeClass.scala
+++ src/main/scala/typeclasses/homebrew/TypeClass.scala
@@ -22,58 +22,57 @@ trait TypeClassImpl[C[_]] {
     instance: C[B],
     to: A => B,
     from: B => A): C[A]
-
 }
 
 trait TypeClass[C[_]] extends TypeClassImpl[C] { tc =>
-  trait ProductRipper[L] extends DepFn0 {
+  trait Instance[L] extends DepFn0 {
     type Inner
     final type Out = C[Inner]
   }
 
-  type ProductAux[L <: HList, I <: HList] = ProductRipper[L] {
+  type ProductAux[L <: HList, I <: HList] = Instance[L] {
     type Inner = I
   }
 
-  type CoproductAux[L <: Coproduct, I <: Coproduct] = ProductRipper[L] {
+  type CoproductAux[L <: Coproduct, I <: Coproduct] = Instance[L] {
     type Inner = I
   }
 
-  type GenericAux[A, B] = ProductRipper[A] {
+  type GenericAux[A, B] = Instance[A] {
     type Inner = B
   }
 
-  implicit def emptyProductRipper[In <: HNil]: ProductAux[In, HNil] =
-    new ProductRipper[In] {
+  implicit def emptyProductInstance[In <: HNil]: ProductAux[In, HNil] =
+    new Instance[In] {
       type Inner = HNil
       def apply() = emptyProduct
     }
 
-  implicit def productRipper[Label <: Symbol, Head, Tail <: HList, TailInner <: HList](
+  implicit def productInstance[Label <: Symbol, Head, Tail <: HList, TailInner <: HList](
     implicit witness: Lazy[Witness.Aux[Label]],
     cHead: Lazy[C[Head]],
-    tailRipper: Lazy[ProductAux[Tail, TailInner]]): ProductAux[FieldType[Label, Head] :: Tail, Head :: TailInner] =
-    new ProductRipper[FieldType[Label, Head]:: Tail] {
+    tailInstance: Lazy[ProductAux[Tail, TailInner]]): ProductAux[FieldType[Label, Head] :: Tail, Head :: TailInner] =
+    new Instance[FieldType[Label, Head]:: Tail] {
       type Inner = Head :: TailInner
-      def apply() = product(witness.value.value.name, cHead.value, tailRipper.value())
+      def apply() = product(witness.value.value.name, cHead.value, tailInstance.value())
     }
 
-  implicit def emptyCoproductRipper[In <: CNil]: CoproductAux[In, CNil] =
-    new ProductRipper[In] {
+  implicit def emptyCoproductInstance[In <: CNil]: CoproductAux[In, CNil] =
+    new Instance[In] {
       type Inner = CNil
       def apply() = emptyCoproduct
     }
 
-  implicit def coproductRipper[Label <: Symbol, Left, Right <: Coproduct, RightInner <: Coproduct](
+  implicit def coproductInstance[Label <: Symbol, Left, Right <: Coproduct, RightInner <: Coproduct](
     implicit witness: Lazy[Witness.Aux[Label]],
     cLeft: Lazy[C[Left]],
-    rightRipper: Lazy[CoproductAux[Right, RightInner]]): CoproductAux[FieldType[Label, Left] :+: Right, Left :+: RightInner] =
-    new ProductRipper[FieldType[Label, Left]:+: Right] {
+    rightInstance: Lazy[CoproductAux[Right, RightInner]]): CoproductAux[FieldType[Label, Left] :+: Right, Left :+: RightInner] =
+    new Instance[FieldType[Label, Left]:+: Right] {
       type Inner = Left :+: RightInner
-      def apply() = coproduct(witness.value.value.name, cLeft.value, rightRipper.value())
+      def apply() = coproduct(witness.value.value.name, cLeft.value, rightInstance.value())
     }
 
-  implicit def genericRipper[A, Repr0, Repr1](
+  implicit def genericInstance[A, Repr0, Repr1](
     implicit lg: Lazy[LabelledGeneric.Aux[A, Repr0]],
     bg: Lazy[Generic.Aux[A, Repr1]],
     ripper: Lazy[GenericAux[Repr0, Repr1]]): C[A] =
