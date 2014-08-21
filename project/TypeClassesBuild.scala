bfc3a3a1ac24cb35ab8ed7d4c86de4fc20153189
EAPierzchalski <e.a.pierzchalski@gmail.com>
2014-08-21 14:35:18 +1000

    Add specs2

--- project/TypeClassesBuild.scala
+++ project/TypeClassesBuild.scala
@@ -6,13 +6,16 @@ object TypeClassesBuild extends Build {
   val baseDependencies = Seq(
     "com.chuusai" %% "shapeless" % "2.1.0-SNAPSHOT",
     "org.scala-lang" % "scala-library" % "2.11.2",
-    "org.scala-lang" % "scala-reflect" % "2.11.2"
+    "org.scala-lang" % "scala-reflect" % "2.11.2",
+    "org.specs2" %% "specs2" % "2.4.1" % "test"
   )
 
   val baseResolvers = Seq(
     Resolver.sonatypeRepo("releases"),
     Resolver.sonatypeRepo("snapshots")
   )
+
+  val baseTestScalacOptions = Seq("-Yrangepos")
 
   lazy val baseSettings =
     Defaults.coreDefaultSettings ++
@@ -21,7 +24,8 @@ object TypeClassesBuild extends Build {
         scalaVersion := "2.11.2",
         incOptions := incOptions.value.withNameHashing(nameHashing = true),
         resolvers ++= baseResolvers,
-        libraryDependencies ++= baseDependencies
+        libraryDependencies ++= baseDependencies,
+        scalacOptions in test ++= baseTestScalacOptions
       )
 
   lazy val typeclasses = Project (
