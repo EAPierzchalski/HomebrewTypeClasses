d718ec4301df7452453205d00e69f3ebd2ca6b57
EAPierzchalski <e.a.pierzchalski@gmail.com>
2014-08-21 16:57:17 +1000

    Finish migrating demos
    Remove old CaseObject and InnerRecursion demos, since they were pretty
    boring

---
+++ src/test/scala/typeclasses/homebrew/parametric/ParametricNestingSpec.scala
@@ -0,0 +1,39 @@
+package typeclasses.homebrew.parametric
+
+import org.specs2.Specification
+import typeclasses.homebrew.{ ShowInstances, Show }
+
+/**
+ * Created by eap on 8/21/14.
+ */
+
+sealed trait Simple
+case class S1(s: String) extends Simple
+case class S2(n: Int, s: Simple) extends Simple
+
+sealed trait Nested
+case class NBase(s: String) extends Nested
+case class NSeq(ss: Seq[Simple]) extends Nested
+
+class ParametricNestingSpec extends Specification {
+  def is = s2"""
+  This is a spec for auto codecs over traits with
+  parametric nested values, i.e. trait trees with members of the form
+
+    A(ts: F[B])
+
+  For some F, B.
+
+  The auto Show typeclass should
+    handle Seq nesting $e1
+"""
+
+  def e1 = {
+    import shapeless._
+    import Show._
+    import ShowInstances.auto._
+
+    val nestedShow = (NSeq(Seq(S1("hi"), S1("lo"), S2(12, S1("rec")))): Nested).show
+    nestedShow shouldEqual "NSeq {ss: Seq(S1 {s: hi}, S1 {s: lo}, S2 {n: 12, s: S1 {s: rec}}}})}"
+  }
+}
